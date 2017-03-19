126adba638e6a07b66314ec7968376fe09f1c48f
McKay Bonham <programmer@mckaybonham.com>
2017-03-19 06:06:36 +0000

    Whoops, forgot to Add all files last commit.

--- scratchpad.js
+++ scratchpad.js
@@ -120,175 +120,67 @@ function tribHelper(n) {
 	return { 'value': value, 'html': div };
 }
 
-var fib = function (n, node) {
-	var tree = fibHelper(n)
-		node.appendChild(tree.html);
-		//node.style = "display: inline-block;";
-	  node.setAttribute("id", "fib");
-}
-
-var pell = function (n, node) {
-	var tree = pellHelper(n)
-		node.appendChild(tree.html);
-		//node.style = "display: inline-block;";
-	  node.setAttribute("id", "pell");
-}
-
-var trib = function (n, node) {
-	var tree = tribHelper(n)
-		node.appendChild(tree.html);
-		//node.style = "display: inline-block;";
-	  node.setAttribute("id", "trib");
-}
-
-var style = document.createElement('style');
-style.textContent = 
-	"#fib {" +
-	"	display: inline-block;" +
-	"	width: 20000px;" +
-	"}" +
-	"" +
-	".fib {" +
-	"	background-color: rgba(0,0,255,0.1);" +
-	"}" +
-	"" +
-	".fib-left {" +
-	"	float: left;" +
-	"	display: inline-block;" +
-	"	margin-right: 4px;" +
-	"}" +
-	"" +
-	".fib-right {" +
-	"	float: right;" +
-	"	display: inline-block;" +
-	"	margin-left: 4px;" +
-	"}" +
-	"#pell {" +
-	"	display: inline-block;" +
-	"	width: 20000px;" +
-	"}" +
-	"" +
-	".pell {" +
-	"	background-color: rgba(0,0,255,0.1);" +
-	"}" +
-	"" +
-	".pell-left {" +
-	"	float: left;" +
-	"	display: inline-block;" +
-	"	margin-right: 4px;" +
-	"}" +
-	"" +
-	".pell-right {" +
-	"	float: right;" +
-	"	display: inline-block;" +
-	"	margin-left: 4px;" +
-	"}" +
-	"#trib {" +
-	"	display: inline-block;" +
-	"	width: 20000px;" +
-	"}" +
-	"" +
-	".trib {" +
-	"	background-color: rgba(0,0,255,0.1);" +
-	"}" +
-	"" +
-	".trib-left {" +
-	"	float: left;" +
-	"	display: inline-block;" +
-	"	margin-right: 4px;" +
-	"}" +
-	".trib-mid {" +
-	"	float: auto;" +
-	"	display: inline-block;" +
-	"	margin-right: 4px;" +
-	"   margin-left: 4px;" +
-	"}" +
-	"" +
-	".trib-right {" +
-	"	float: right;" +
-	"	display: inline-block;" +
-	"	margin-left: 4px;" +
-	"}" +
-	"" +
-	".shadowed {" +
-	"	text-shadow: 1px 1px 2px black;" +
-	"	color:       white;" +
-	"}" +
-	".stuff-box {" +
-	"	font-family: 'helvetica neue', helvetica, sans-serif;" +
-	"	letter-spacing: 1px;" +
-	"	text-transform: capitalize;" +
-	"	text-align: center;" +
-	"	padding: 3px 10px;" +
-	"	margin: 10px;" +
-	"	cursor: pointer;" +
-	"	border-radius: 10px;" +
-	"	border-width: 2px;" +
-	"	border-style: solid;" +
-	"}" +
-	"" +
-	".red {" +
-	"	border-color: rgb(255,0,0);" +
-	"	background:   rgb(180,60,60);" +
-	"	box-shadow: 1px 1px 2px rgba(200,0,0,0.4);" +
-	"}" +
-	"" +
-	".yellow {" +
-	"	border-color: rgb(255,255,0);" +
-	"	background:   rgb(180,180,60);" +
-	"	box-shadow: 1px 1px 2px rgba(200,200,0,0.4);" +
-	"}" +
-	"" +
-	".blue {" +
-	"	border-color: rgb(0,0,255);" +
-	"	background:   rgb(60,60,180);" +
-	"	box-shadow: 1px 1px 2px rgba(0,0,200,0.4);" +
-	"}" +
-	"" +
-	".green {" +
-	"	border-color: rgb(0,255,0);" +
-	"	background:   rgb(60,180,60);" +
-	"	box-shadow: 1px 1px 2px rgba(0,200,0,0.4);" +
-	"}";
-
-document.querySelector('body').appendChild(style);
-
-// IIFE = Immediately-invoked function expression
-// An anonymous function which we create, invoke, then throw away.
-// It exists to meet these aims:
-//   0) Avoid clobbering an existing variable named "div" in the global namespace. I don't have
-//      another variable called "div" today, but this protects my webpage in the event that I
-//      later incorporate other JavaScript programs from elsewhere.
-//   1) Collect a few related program statements and keep them together in a single unit.
-// https://en.wikipedia.org/wiki/Immediately-invoked_function_expression
-( function(color, id) {
-	var div = document.createElement('div');
-	div.setAttribute('class', color + ' shadowed stuff-box');
-	div.setAttribute('id', id);
-	document.body.appendChild(div);
-}('red', 'fib'));
-
-fib(11, document.querySelector('.red'))
 
 
-// divMakerMaker() is a function which returns a function
-// divMakerMaker() takes two arguments and creates a function which requires
-// no arguments of its own, but upon invocation "remembers" the functions it
-// was created with
-var divMakerMaker = function(color, id) {
-	return function() {
-		var div = document.createElement('div');
-		div.setAttribute('class', color + ' shadowed stuff-box');
-		div.setAttribute('id', id);
-		document.body.appendChild(div);
+var fib = function(n, node) {
+	var fibTree = node.querySelector('div.fib');
+	if (fibTree) {
+		node.removeChild(fibTree);
 	}
+	var tree = fibHelper(n);
+	node.appendChild(tree.html);
+}
+var fibButton = function(me) {
+	var form = me.parentNode;
+	var slider = form.querySelector('input');
+	var value = slider.value;
+	// console.log(value);
+	fib(parseInt(value), form.parentNode);
+}
+var fibSlider = function(me) {
+	var form = me.parentNode;
+	var button = form.querySelector('button');
+	button.textContent = 'fib(' + me.value + ')';
 }
 
-var blueDiv = divMakerMaker('blue', 'pell');
-var yellowDiv = divMakerMaker('yellow', 'trib');
+var pell = function(n, node) {
+	var pellTree = node.querySelector('div.pell');
+	if (pellTree) {
+		node.removeChild(pellTree);
+	}
+	var tree = pellHelper(n);
+	node.appendChild(tree.html);
+}
+var pellButton = function(me) {
+	var form = me.parentNode;
+	var slider = form.querySelector('input');
+	var value = slider.value;
+	// console.log(value);
+	pell(parseInt(value), form.parentNode);
+}
+var pellSlider = function(me) {
+	var form = me.parentNode;
+	var button = form.querySelector('button');
+	button.textContent = 'pell(' + me.value + ')';
+}
 
-blueDiv();
-yellowDiv();
-
-pell(11, document.querySelector('.blue'))
-trib(11, document.querySelector('.yellow'))
+var trib = function(n, node) {
+	var tribTree = node.querySelector('div.trib');
+	if (tribTree) {
+		node.removeChild(tribTree);
+	}
+	var tree = tribHelper(n);
+	node.appendChild(tree.html);
+}
+var tribButton = function(me) {
+	var form = me.parentNode;
+	var slider = form.querySelector('input');
+	var value = slider.value;
+	// console.log(value);
+	trib(parseInt(value), form.parentNode);
+}
+var tribSlider = function(me) {
+	var form = me.parentNode;
+	var button = form.querySelector('button');
+	button.textContent = 'trib(' + me.value + ')';
+}
